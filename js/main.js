fb9e6466405d95a277ec386effe994b1828887a8
chitra4git <message2chitra@gmail.com>
2023-02-01 09:19:58 -0700

    updated project

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const menuBtn = document.querySelector('.hamburger');
+const menuBar = document.querySelector('.menu-bar');
+const menuList = document.querySelector('.nav-menu');
+
+menuBtn.addEventListener('click', showMenu);
+
+function showMenu(){
+    menuBtn.classList.toggle('is-active');
+    menuBar.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+}
+
+// form contact us starts
+
+// Input Lock
+$('textarea').blur(function () {
+    $('#hire textarea').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('textarea + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('textarea + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:first-child input').blur(function () {
+    $('#hire .field:first-child input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:nth-child(2) input').blur(function () {
+    $('#hire .field:nth-child(2) input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+
+// work experience
+
+new WOW().init();
+
+
+  	/* AUTHOR LINK */
+     $('.about-me-img').hover(function(){
+            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+        }, function(){
+            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+        });
